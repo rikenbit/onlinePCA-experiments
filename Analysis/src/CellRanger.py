3c3221b337ad3d59e86402ee9c798a452803d5c0
kokitsuyuzaki <k.t.the-answer@hotmail.co.jp>
2019-05-18 19:57:07 +0900

    CellRanger

--- Analysis/src/CellRanger.py
+++ Analysis/src/CellRanger.py
@@ -76,13 +76,11 @@ def irlb(A, n, tol=0.0001, maxit=50, center=None, scale=None, random_state=0):
     A.dot(X[2]) = X[0]*X[1].
     """
     np.random.seed(random_state)
-
     # Numpy routines do undesirable things if these come in as N x 1 matrices instead of size N arrays
     if center is not None and not isinstance(center, np.ndarray):
         raise TypeError("center must be a numpy.ndarray")
     if scale is not None and not isinstance(scale, np.ndarray):
         raise TypeError("scale must be a numpy.ndarray")
-
     nu = n
     m = A.shape[0]
     n = A.shape[1]
@@ -97,45 +95,35 @@ def irlb(A, n, tol=0.0001, maxit=50, center=None, scale=None, random_state=0):
     j = 0
     k = nu
     smax = 1
-
     V = np.zeros((n, m_b)) # Approximate right vectors
     W = np.zeros((m, m_b)) # Approximate left vectors
     F = np.zeros((n, 1)) # Residual vector
     B = np.zeros((m_b, m_b)) #Bidiagonal approximation
-
     V[:, 0] = np.random.randn(n)  # Initial vector
     V[:, 0] = V[:, 0] / np.linalg.norm(V)
-
     while(it < maxit):
         if(it > 0):
             j = k
-
         VJ = V[:, j]
-
         # apply scaling
         if scale is not None:
             VJ = VJ / scale
-
         W[:, j] = mult(A, VJ)
         mprod += 1
-
         # apply centering
         # R code: W[, j_w] <- W[, j_w] - ds * drop(cross(dv, VJ)) * du
         if center is not None:
             W[:, j] = W[:, j] - np.dot(center, VJ)
-
         if(it > 0):
             # NB W[:,0:j] selects columns 0,1,...,j-1
             W[:, j] = orthog(W[:, j], W[:, 0:j])
         s = np.linalg.norm(W[:, j])
         sinv = invcheck(s)
         W[:, j] = sinv * W[:, j]
-
         # Lanczos process
         while(j < m_b):
             F = mult(A, W[:, j], t=True)
             mprod += 1
-
             # apply scaling
             if scale is not None:
                 F = F / scale
@@ -154,19 +142,15 @@ def irlb(A, n, tol=0.0001, maxit=50, center=None, scale=None, random_state=0):
                 B[j, j] = s
                 B[j, j + 1] = fn
                 VJp1 = V[:, j + 1]
-
                 # apply scaling
                 if scale is not None:
                     VJp1 = VJp1 / scale
-
                 W[:, j + 1] = mult(A, VJp1)
                 mprod += 1
-
                 # apply centering
                 # R code: W[, jp1_w] <- W[, jp1_w] - ds * drop(cross(dv, VJP1)) * du
                 if center is not None:
                     W[:, j + 1] = W[:, j + 1] - np.dot(center, VJp1)
-
                 # One step of classical Gram-Schmidt...
                 W[:, j + 1] = W[:, j + 1] - fn * W[:, j]
                 # ...with full reorthogonalization
@@ -202,12 +186,38 @@ def irlb(A, n, tol=0.0001, maxit=50, center=None, scale=None, random_state=0):
         # Update the left approximate singular vectors
         W[:, 0:k] = W[:, 0:m_b].dot(S[0][:, 0:k])
         it += 1
-
     U = W[:, 0:m_b].dot(S[0][:, 0:nu])
     V = V[:, 0:m_b].dot(S[2].transpose()[:, 0:nu])
     return((U, S[1][0:nu], V, it, mprod))
 
 # https://github.com/10XGenomics/cellranger/blob/master/lib/python/cellranger/analysis/irlb.py
+
+def normalize_and_transpose(matrix):
+    matrix.tocsc()
+    # m = normalize_by_umi(matrix)
+    # Use log counts
+    m = matrix
+    m.data = np.log10(1 + m.data) # modified log2 => log10
+    # Transpose
+    m = m.T
+    # compute centering (mean) and scaling (stdev)
+    (c,v) = summarize_columns(m)
+    # TODO: Inputs to this function shouldn't have zero variance columns
+    v[np.where(v == 0.0)] = 1.0
+    s = np.sqrt(v)
+    return (m, c, s)
+
+# https://github.com/10XGenomics/cellranger/blob/5f5a6293bbc067e1965e50f0277286914b96c908/lib/python/cellranger/analysis/pca.py
+
+def summarize_columns(matrix):
+    ''' Calculate mean and variance of each column, in a sparsity-preserving way.'''
+    mu = matrix.mean(axis=0).A
+    # sparse variance = E(col^2) - E(col)^2
+    mu2 = matrix.multiply(matrix).mean(axis=0).A
+    var = mu2 - mu**2
+    return mu, var
+
+# https://github.com/10XGenomics/cellranger/blob/5f5a6293bbc067e1965e50f0277286914b96c908/lib/python/cellranger/analysis/stats.py
 
 
 # args[1] : Input file
@@ -220,9 +230,10 @@ args = sys.argv
 data = np.loadtxt(args[1], delimiter=",")
 
 # PCA
-A = sp.csr_matrix(data)
 dim = int(args[2])
-out = irlb(A, n=dim)
+A = sp.csr_matrix(data)
+(pca_norm_mat, pca_center, pca_scale) = normalize_and_transpose(A)
+out = irlb(pca_norm_mat, n=dim, center=pca_center.squeeze())
 
 # Save
 np.savetxt(args[3], out[0], delimiter=",")
